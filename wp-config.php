9a8e1ced960f57a0ed7e7e4e108164f9839bf76e
shadowscan-testing <hacktheplanet0x@gmail.com>
2026-01-12 15:36:59 +0530

    Create wp-config.php
    This is a Sample file for Educational Testing Purpose.

---
+++ wp-config.php
@@ -0,0 +1,15 @@
+<?php
+define('DB_NAME', 'wordpress_db');
+define('DB_USER', 'wp_admin');
+define('DB_PASSWORD', 'WpAdmin@123');
+define('DB_HOST', 'localhost');
+
+define('AUTH_KEY', 'fake-auth-key');
+define('SECURE_AUTH_KEY', 'fake-secure-auth-key');
+define('LOGGED_IN_KEY', 'fake-logged-in-key');
+define('NONCE_KEY', 'fake-nonce-key');
+
+$table_prefix = 'wp_';
+
+define('WP_DEBUG', true);
+?>
